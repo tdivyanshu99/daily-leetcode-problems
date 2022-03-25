b559bccf3460fbcda50047e0fc366eae86d8eacd
Divyanshu Tiwari <tdivyanshu99@gmail.com>
2022-03-26 03:42:21 +0530

    added two_sum_2 code

---
+++ day1/two_sum_2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1;
+        while(i<j){
+            if(nums[i]+nums[j]>target) j--;
+            else if(nums[i]+nums[j]==target) return {i+1,j+1};
+            else{
+                i++;
+            }
+        }
+        return {};
+    }
+};
