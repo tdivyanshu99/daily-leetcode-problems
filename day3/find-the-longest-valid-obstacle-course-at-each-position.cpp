8b961553253fe075c94f79090b8991c16ac47c6d
Divyanshu Tiwari <tdivyanshu99@gmail.com>
2023-05-07 23:32:25 +0530

    Added 2nd question

---
+++ day3/find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v;
+        vector<int> res(n);
+        for(int i=0;i<nums.size();i++) {
+            int idx=upper_bound(v.begin(),v.end(),nums[i]) - v.begin();
+            if(idx==v.size()) {
+                v.push_back(nums[i]);
+                res[i]=idx+1;
+            }
+            else {
+                v[idx]=nums[i];
+                res[i]=idx+1;
+            }
+        }
+        return res;
+    }
+};
+// 1 2 3 2
